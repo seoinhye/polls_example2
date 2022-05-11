e339f11b738dbe391f2deb4edc44561c1b7e604c
seoinhye <dlsgp30@naver.com>
2022-05-11 15:50:29 +0900

    Initial commit

---
+++ config/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from polls import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls'))
+]
+
